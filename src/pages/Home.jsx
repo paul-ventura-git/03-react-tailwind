4879d995eb969f51ab7aa02fd01c14fda9a12fea
Paul Ventura <j.paul.ventura@gmail.com>
2024-05-02 15:32:48 -0500

    LogoClouds

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
 import HeaderSection from '../components/HeaderSection'
+import LogoClouds  from '../components/LogoClouds'
 
 export const Home = () => {
   return (
     <div>
         <HeaderSection></HeaderSection>
+        <LogoClouds></LogoClouds>
     </div>
   )
 }
