1b61593c1ff7b1bf47bd2a37595d5aa8d9b1f64d
Paul Ventura <j.paul.ventura@gmail.com>
2024-05-02 15:28:40 -0500

    FeatureSection

--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import SlideOver from '../components/SlideOver'
 import CompleteForm from '../components/CompleteForm'
+import FeatureSection from '../components/FeatureSection.jsx'
 
 export const Features = () => {
   return (
     <div className='my-form'>
-        <CompleteForm></CompleteForm>
+        <FeatureSection></FeatureSection>
     </div>
   )
 }
